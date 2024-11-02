0ad1e75592e85c2fb2ace8eccf15d095fac60791
yugang123 <104823200+yugang123@users.noreply.github.com>
2024-11-03 04:29:25 +0800

    Update __init__.py

--- evalplus/eval/__init__.py
+++ evalplus/eval/__init__.py
@@ -238,8 +238,8 @@ def untrusted_check(
         timeout += 1  # extra time for data collection
 
     # shared memory objects
-    progress = Value("i", 0)
-    stat = Value("i", _UNKNOWN)
+    progress = 0
+    stat = _UNKNOWN
     details = Array("b", [False for _ in range(len(inputs))])
 
     p = multiprocessing.Process(
@@ -269,9 +269,8 @@ def untrusted_check(
         time.sleep(0.1)
 
     stat = _mapping[stat.value]
-    progress.value=len(inputs)
-    details = details[: progress.value]
-    if not stat or stat==FAIL:
+    details = details[: len(inputs)]
+    if not stat:
         stat = TIMEOUT
 
     if stat == PASS:
