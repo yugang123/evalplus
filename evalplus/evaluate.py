7219154ed37e7e216d7c598e796d5821fef51475
yugang123 <104823200+yugang123@users.noreply.github.com>
2024-11-03 14:57:56 +0800

    Update evaluate.py

--- evalplus/evaluate.py
+++ evalplus/evaluate.py
@@ -1,5 +1,3 @@
-import sys
-sys.set_int_max_str_digits(50000)
 import json
 import multiprocessing
 import os
@@ -268,11 +266,11 @@ def evaluate(
                     else:
                         if(baseorplus=='base'):
                             return [
-                                expected_output[task_id]["base"][i] for i in range(len(details)) if not details[i]
+                                str(expected_output[task_id]["base"][i]) for i in range(len(details)) if not details[i]
                             ]
                         if(baseorplus=='plus'):
                             return [
-                                expected_output[task_id]["plus"][i] for i in range(len(details)) if not details[i]
+                                str(expected_output[task_id]["plus"][i]) for i in range(len(details)) if not details[i]
                             ]
                 base_stat, base_details = res["base"]
                 base_fail_tests = get_failed_tests(
