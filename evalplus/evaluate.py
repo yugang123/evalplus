4b5066c1670e94b472cb07c0e99d08c0e538b566
yugang123 <104823200+yugang123@users.noreply.github.com>
2024-11-03 16:11:29 +0800

    Update evaluate.py

--- evalplus/evaluate.py
+++ evalplus/evaluate.py
@@ -257,7 +257,7 @@ def evaluate(
 
                     else:
                         return [
-                            inputs[i] for i in range(len(inputs)) if not details[i]
+                            inputs[i] for i in range(len(details)) if not details[i]
                         ]
                 threshold = 1e6  # 设置阈值，超大整数的标准
                 getcontext().prec = 50
